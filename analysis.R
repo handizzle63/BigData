0f5a9a2330f32288510d4e4a59bd0ad10789f4f6
handizzle63 <hannah.williams63@live.co.uk>
2023-10-24 15:33:46 +0800

    analysis on lure 20m strength

--- analysis.R
+++ analysis.R
@@ -4,6 +4,9 @@
 library(dplyr)
 library(ggplot2)
 library(cowplot)
+library(car)
+library(foreign)
+library(RColorBrewer)
 
 #read in data & creating RDS
 # grid <- read.csv('simulations/grid_l20.csv')
@@ -55,20 +58,24 @@ library(cowplot)
 ## difference between 1000 or 5000 simulations?
 dat <- readRDS('flies1.RDS')
 
+
 # summary(dat)
 # 
-# kilo <- dat %>%
-#   dplyr::filter(sim < 1001)%>%
-#   glimpse()
-# tail(kilo)
-# 
-# meank <- tapply(kilo$time, kilo$type, mean)
-# meank
-# sdk <- tapply(kilo$time, kilo$type, sd)
-# sdk
-# medk <- tapply(kilo$time, kilo$type, median)
-# medk
-# 
+kilo <- dat %>%
+  dplyr::filter(sim < 1001)%>%
+  glimpse()
+tail(kilo)
+
+meank <- tapply(kilo$ntrees, kilo$type, mean)
+meank
+sdk <- tapply(kilo$ntrees, kilo$type, sd)
+sdk
+medk <- tapply(kilo$ntrees, kilo$type, median)
+medk
+
+aggregate(kilo, by=list(kilo$type), mean)
+aggregate(kilo, by=list(kilo$type), sd)
+
 # meandat <- tapply(dat$time, dat$type, mean)
 # meandat
 # sddat <- tapply(dat$time, dat$type, sd)
@@ -77,30 +84,75 @@ dat <- readRDS('flies1.RDS')
 # meddat
 
 
-
 ##############################################
 #K-S Test 
+dat <- dat%>%
+  dplyr::filter(sim < 1001)%>%
+  glimpse()
 
-ks.test(dat$time~dat$type)
+ks.test(dat$time~dat$type) #grouping factor == 2 levels
 
 #KS between grid and each other surveillance type?
 df <- dat %>%
-  dplyr::filter(type %in% c("grid", "redgrid"))
+  dplyr::filter(type %in% c("grid", "ineff"))%>%
+  glimpse()
 
+unique(df$type)
+
+ks.test(df$time~df$type)
 
 #plot ecdf of each surveillance type on the y with x = years
-ecdf(grid$time)
 
-sample.data = read.table ('data.txt', header = TRUE, sep = "\t")
-cdf <- ggplot (data=sample.data, aes(x=Delay, group =Type, color = Type)) + stat_ecdf()
-cdf
+cdf.time <- ggplot(
+   data=dat, aes(x=time, group = type, col = type)) +
+   stat_ecdf()
+  
+cdf.time
 
 #number of trees infested at first detection
 
+cdf.trees <- ggplot(
+  data=dat, aes(x=ntrees, group = type, col = type)) +
+  stat_ecdf()
+
+cdf.trees
 
 
-# y= ecdf, x = max distance
+# y= ecdf, x = males
+cdf.males <- ggplot(
+  data=dat, aes(x=nmales, group = type, col = type)) +
+  stat_ecdf()
+
+cdf.males
 
 
+####################### Comparing means - ANOVA
+mod <- aov(ntrees~type, data = dat)
+Anova(mod)
+resid <- residuals(mod)
+resid
+plot(resid) #normal distribution
+qplot(resid) 
 
-# y = ecdf, x = total area infested
+tuk <- TukeyHSD(mod, conf.level=.95)
+plot(tuk)
+tuk
+
+##################
+## 90% worst case scenario
+
+df <- dat %>%
+  dplyr::filter(type %in% c("grid"))%>%
+  glimpse()
+mean(df$time)
+summary(df)
+
+summary(dat)
+
+df <- dat %>%
+  dplyr::filter(time < 88) %>%
+  glimpse()
+
+9000/100
+90*99 #8910
+90*90 #8100
